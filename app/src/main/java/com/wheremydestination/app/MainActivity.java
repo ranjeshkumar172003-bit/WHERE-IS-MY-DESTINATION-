e7ae1e74a5708a121b6a130a805a9cf8c1bcb002
ranjeshkumar172003-bit <ranjeshkumar172003@gmail.com>
2026-08-27 12:45:55 +0530

    Update package name and enable DOM storage

--- app/src/main/java/com/wheremydestination/app/MainActivity.java
+++ app/src/main/java/com/wheremydestination/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.whereismydestination; // Apne project ka package name check kar lein
+package com.wheremydestination.app;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -7,16 +7,21 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         WebView webView = new WebView(this);
+
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
         webView.setWebViewClient(new WebViewClient());
-        
+
         webView.loadUrl("file:///android_asset/index.html");
+
         setContentView(webView);
     }
 }
